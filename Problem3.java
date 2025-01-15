86c9a4b688c7dfb44ceee6470ebe388d1887ea5f
rahesh <66882038+rahesh808@users.noreply.github.com>
2025-01-15 11:49:18 +0530

    Added problem3

---
+++ Problem3.java
@@ -0,0 +1,20 @@
+
+class Solution2 {
+    public int maxArea(int[] height) {
+        if (height == null || height.length < 2) {
+            return 0;
+        }
+        int max = 0;
+        int left = 0;
+        int right = height.length - 1;
+        while (left < right) {
+            max = Math.max(max, Math.min(height[left], height[right]) * (right - left));
+            if (height[left] <= height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return max;
+    }
+}
